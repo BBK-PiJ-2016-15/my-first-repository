005a3f77ff365e30fbd8615d1785730e203510be
BBK-PiJ-2016-15 <fghers01@dcs.bbk.ac.uk>
2016-10-10 20:17:25 +0100

    lines and messages added

--- test_code.groovy
+++ test_code.groovy
@@ -3,5 +3,21 @@ println "Type a number"
 a = System.console()readLine()
 int b = Integer.parseInt(a)
 
+println a + " * 1 = "+ b * 1 
+println a + " * 2 = "+ b * 2 
+println a + " * 3 = "+ b * 3 
+println a + " * 4 = "+ b * 4 
+println a + " * 5 = "+ b * 5 
+println a + " * 6 = "+ b * 6 
+println a + " * 7 = "+ b * 7 
+println a + " * 8 = "+ b * 8 
+println a + " * 9 = "+ b * 9 
+println a + " * 10 = "+ b * 10 
 
-println a + " x 2 = "+ b * 2 
+tenth = b * 10
+
+if (tenth > 1000) {
+	println a +" * 10 is greater than 1000"
+}
+else{
+	println a + " * 10 is smaller than 1000"}
